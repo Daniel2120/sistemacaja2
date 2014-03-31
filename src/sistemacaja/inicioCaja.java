0d69ea6c3323f2f3c32fbce9f304d7ae6221ed67
ArchivoRegionalPuno <arp@arp.org.pe>
2014-03-31 15:00:56 -0500

    actualizar

--- src/sistemacaja/inicioCaja.java
+++ src/sistemacaja/inicioCaja.java
@@ -1,16 +1,30 @@
 package sistemacaja;
 
+import java.util.TimerTask;
+import javax.swing.JOptionPane;
+import javax.swing.Timer;
+
 /**
  *
  * @author Planificacion1
  */
 public class inicioCaja extends javax.swing.JFrame {
 
+   
+
+       
+
+    
     /**
      * Creates new form inicioCaja
      */
     public inicioCaja() {
         initComponents();
+        
+        this.setLocationRelativeTo(null);
+       
+         
+       
     }
 
     /**
@@ -22,31 +36,19 @@ public class inicioCaja extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jButton1 = new javax.swing.JButton();
         jLabel4 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setFocusCycleRoot(false);
+        setLocationByPlatform(true);
+        setUndecorated(true);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/siscajaImagenes/boton_4_3.png"))); // NOI18N
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
-        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 160, 290, 40));
-
         jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/siscajaImagenes/FONDO.png"))); // NOI18N
-        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 640, 360));
+        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 630, 360));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        frmLogin Login=new frmLogin();
-        Login.setVisible(true);
-        dispose();
-    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -84,7 +86,6 @@ public class inicioCaja extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel4;
     // End of variables declaration//GEN-END:variables
 }
